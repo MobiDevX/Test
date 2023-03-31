f13dda85a59b505334b80b5921c576f787d602b8
MobDe <MobDevX.outlook.com>
2023-03-31 13:13:23 +0700

    object CounterSingleton

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -13,60 +13,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun OnButtonClick(view: View){
-        printMovies(NoMoviesList)
-        printMovies(emptyList())
+        for (i in 1 until 5) {
+            var text = CounterSingleton.increment().toString()
+            Toast.makeText(this, text, Toast.LENGTH_SHORT)
+                .show()
+        }
         Toast.makeText(this, R.string.correct_toast,
             Toast.LENGTH_SHORT)
             .show()
     }
 
-    fun printMovies(movies: List<String>) {
-        for (m in movies) {
-            println(m)
+    object CounterSingleton {
+        init {
+            println("Ко мне обратились впервые")
         }
+        private var counter = 1
+        fun increment() = counter++
     }
 
-    object NoMoviesList : List<String> {
-        override val size = 0
-        override fun get(index: Int): String {
-            TODO("Not yet implemented")
-        }
+    fun demo() {
 
-        override fun isEmpty(): Boolean = true
-
-        override fun iterator(): Iterator<Nothing> = EmptyIterator
-
-        override fun listIterator(): ListIterator<String> {
-            TODO("Not yet implemented")
-        }
-
-        override fun listIterator(index: Int): ListIterator<String> {
-            TODO("Not yet implemented")
-        }
-
-        override fun subList(fromIndex: Int, toIndex: Int): List<String> {
-            TODO("Not yet implemented")
-        }
-
-        override fun lastIndexOf(element: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        override fun indexOf(element: String): Int {
-            TODO("Not yet implemented")
-        }
-
-        internal object EmptyIterator : ListIterator<Nothing> {
-            override fun hasNext(): Boolean = false
-            override fun hasPrevious(): Boolean = false
-            override fun nextIndex(): Int = 0
-            override fun previousIndex(): Int = -1
-            override fun next(): Nothing = throw NoSuchElementException()
-            override fun previous(): Nothing = throw NoSuchElementException()
-        }
-
-        override fun containsAll(elements: Collection<String>) = false
-
-        override fun contains(element: String) = false
     }
 }
