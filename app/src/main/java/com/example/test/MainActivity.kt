4ef5c3f5b2b638892870f0044298adfc92858591
MobDe <MobDevX.outlook.com>
2023-03-31 11:49:13 +0700

    Add Singleton

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun OnButtonClick(view: View){
+        Toast.makeText(this, R.string.correct_toast,
+            Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    fun printMovies(movies: List<String>) {
+        for (m in movies) {
+            println(m)
+        }
     }
 }
