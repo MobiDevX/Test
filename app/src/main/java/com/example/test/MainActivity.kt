7bce02a94b9949f193cbe49797f10c96855dc04d
MobDe <MobDevX.outlook.com>
2023-03-31 12:59:34 +0700

    object NoMoviesList

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -13,6 +13,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun OnButtonClick(view: View){
+        printMovies(NoMoviesList)
+        printMovies(emptyList())
         Toast.makeText(this, R.string.correct_toast,
             Toast.LENGTH_SHORT)
             .show()
@@ -23,5 +25,48 @@ class MainActivity : AppCompatActivity() {
             println(m)
         }
     }
-    object NoMoviesList
+
+    object NoMoviesList : List<String> {
+        override val size = 0
+        override fun get(index: Int): String {
+            TODO("Not yet implemented")
+        }
+
+        override fun isEmpty(): Boolean = true
+
+        override fun iterator(): Iterator<Nothing> = EmptyIterator
+
+        override fun listIterator(): ListIterator<String> {
+            TODO("Not yet implemented")
+        }
+
+        override fun listIterator(index: Int): ListIterator<String> {
+            TODO("Not yet implemented")
+        }
+
+        override fun subList(fromIndex: Int, toIndex: Int): List<String> {
+            TODO("Not yet implemented")
+        }
+
+        override fun lastIndexOf(element: String): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun indexOf(element: String): Int {
+            TODO("Not yet implemented")
+        }
+
+        internal object EmptyIterator : ListIterator<Nothing> {
+            override fun hasNext(): Boolean = false
+            override fun hasPrevious(): Boolean = false
+            override fun nextIndex(): Int = 0
+            override fun previousIndex(): Int = -1
+            override fun next(): Nothing = throw NoSuchElementException()
+            override fun previous(): Nothing = throw NoSuchElementException()
+        }
+
+        override fun containsAll(elements: Collection<String>) = false
+
+        override fun contains(element: String) = false
+    }
 }
